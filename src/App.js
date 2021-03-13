362236a661b0e50d4ea1ed6cd66b8498bc3ecb69
Pedro Moreira <pedrombm19@gmail.com>
2021-03-13 18:35:48 +0000

    theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
+
 const App = () => {
-  return <div>API Gateway Picker</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>API Gateway Picker</div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
