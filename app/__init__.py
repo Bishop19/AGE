dacf2ce341d090452a38cafec202d3481fe44aab
Pedro Moreira <pedrombm19@gmail.com>
2021-05-10 18:32:29 +0100

    Cors (#2)

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
-
+from flask_cors import CORS
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+CORS(app)
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 migrate = Migrate(app, db)
